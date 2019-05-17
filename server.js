9e0eec8cda4482c3ae57fce299ea70a1f393a204
DavidLevayer <david.levayer@gmail.com>
2019-05-17 13:45:43 +0200

    use RabbitmqManager from common

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 'use strict';
 
-const RabbitmqConsumerInfo = require('spread-common/libs/model/rabbitmq/rabbitmq-consumer-info');
-const RabbitmqConsumer = require('spread-common/libs/util/rabbitmq/rabbitmq-consumer');
+const RabbitmqInfo = require('spread-common/libs/model/rabbitmq/rabbitmq-info');
+const RabbitmqManager = require('spread-common/libs/util/rabbitmq/rabbitmq-manager');
 const ResourceConsumer = require('./libs/core/consumer/resource.consumer');
 
-const rabbitmqConsumerInfo = new RabbitmqConsumerInfo('localhost', 5672, 'spread');
-const rabbitmqConsumer = new RabbitmqConsumer(rabbitmqConsumerInfo);
+const rabbitmqInfo = new RabbitmqInfo('localhost', 5672, 'spread');
+const rabbitmqManager = new RabbitmqManager(rabbitmqInfo);
 
-const resourceConsumer = new ResourceConsumer();
-
-setTimeout(() => {
-    rabbitmqConsumer.setConsumer(resourceConsumer);
-}, 2000);
+// Register consumers
+rabbitmqManager.addConsumer(new ResourceConsumer());
